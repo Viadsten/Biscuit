d2e3a99194bce0f39c31b8bf1e450ce50bac952c
Viadsten <stanulevich.vg@yandex.ru>
2021-02-07 01:41:02 +0300

    fix bugs

--- js/script.js
+++ js/script.js
@@ -16,20 +16,20 @@ $( document ).ready(function() {
   
         $(window).resize(function() {
           if(document.documentElement.clientWidth < 940) {
-            $('.header-nav').slideUp();
+            $('.header-nav').slideUp(0);
             $('.header-toggle').removeClass('toggle--active');
           }
         });
           cachedWidth = newWidth;
       }
   });
-  if ($(window).width() < 940){
+  
     $('.header-nav').slideUp(0);
     $( ".header-toggle" ).click(function() {
         $('.header-nav').slideToggle();
         $('.header-toggle').toggleClass('toggle--active');
 
     });
-  }
+  
 
 });
