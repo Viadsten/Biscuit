8647f2e199f074f2f33cfe1a82cace10fcca1033
Viadsten <stanulevich.vg@yandex.ru>
2021-02-07 03:57:52 +0300

    fix card image

--- js/script.js
+++ js/script.js
@@ -23,13 +23,13 @@ $( document ).ready(function() {
           cachedWidth = newWidth;
       }
   });
-  
+  if ($(window).width() < 940){
     $('.header-nav').slideUp(0);
     $( ".header-toggle" ).click(function() {
         $('.header-nav').slideToggle();
         $('.header-toggle').toggleClass('toggle--active');
 
     });
-  
+  }
 
 });
